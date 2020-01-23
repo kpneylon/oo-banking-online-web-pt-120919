307c00ee709e4e586a6fa7328cbb390c6e32bd30
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-01-23 15:17:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,18 +14,15 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
-    elsif
-      @status = "rejected"
-      "Transaction rejected. Please check your account balance."
-    else
-      BankAccount.status = "closed"
-      "Transaction rejected. Please check your account balance."
+      if @sender.balance > @amount && @status == "pending" && valid?
+        @sender.balance -= @amount
+        @receiver.balance += @amount
+        @status = "complete"
+      else 
+        @status = "rejected"
+        return "Transaction rejected. Please check your account balance."
+      end
     end
-  end
 
     def reverse_transfer
       if @status == "complete"
